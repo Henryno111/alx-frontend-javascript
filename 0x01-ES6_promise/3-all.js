7e43bc9617333cf7259ebd779f1e889a1beca88e
henryno111 <h.agukwe@yahoo.com>
2024-04-15 11:49:59 +0100

    changed to one temporal string to log three resolved promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,14 @@
+/* eslint-disabled */
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  return new Promise((resolve, reject) => {
-    uploadPhoto().then((photoResponse) => {
-      createUser().then((userResponse) => {
-        resolve(`${userResponse.firstName} ${userResponse.lastName}`);
-      }).catch((error) => {
-        console.error('Error creating user:', error);
-        reject('Signup system offline');
-      });
-    }).catch((error) => {
-      console.error('Error uploading photo:', error);
-      reject('Signup system offline');
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResult, userResult]) => {
+      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+    })
+    .catch((error) => {
+      console.error('Signup system offline');
     });
-  });
 }
+
+handleProfileSignup();
