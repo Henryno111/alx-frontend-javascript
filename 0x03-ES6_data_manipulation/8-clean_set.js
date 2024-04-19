358af6836ee33d47587463482af9a605edd5cb2b
henryno111 <h.agukwe@yahoo.com>
2024-04-19 17:13:03 +0100

    retry

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,7 @@
 export default function cleanSet(mySet, startString) {
-
   const filteredValues = Array.from(mySet)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length));
-
 
   return filteredValues.join('-');
 }
