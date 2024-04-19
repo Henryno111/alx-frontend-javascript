6a998ca41979eba78a2174fde3286a25b2c2182e
henryno111 <h.agukwe@yahoo.com>
2024-04-19 13:03:58 +0100

    removed second line logging empty string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
-/* eslint-disabled */
 export default function cleanSet(mySet, startString) {
-  // Use filter to create a new array with elements starting with startString
-  const filteredValues = Array.from(mySet).filter((value) => value.startsWith(startString))
+
+  const filteredValues = Array.from(mySet)
+    .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length));
 
-  // Join the filtered values with a hyphen (-) separator
+
   return filteredValues.join('-');
 }
